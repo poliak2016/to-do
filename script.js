92e6eb81b6f077258902e29c7217f2cbc14e9784
poliak2016 <poliak2016@outlook.com>
2025-08-28 17:45:21 +0200

    Refactor HTML structure and implement save functionality in JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const textForm = document.getElementById("text-form")
+const textForm = document.getElementById("text-form");
+const saveBtn = document.getElementById("save");
+const output = document.getElementById("output");
+
+saveBtn.addEventListener('click', () => {
+  const text = textForm.value.trim();
+
+  if(!text) {
+    return alert("enter any text");
+  }
+
+  const li = document.createElement("li");
+  li.textContent = text;
+  output.appendChild(li)
+});
+
